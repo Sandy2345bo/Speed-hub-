7f3027aeb0629aba979c83912e4c7849f9720a0e
Sandy2345bo <mvvv11082011@gmail.com>
2024-11-11 12:24:55 +0500

    Speed hub.lua

---
+++ Speed hub.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://darkscripts.space/scripts/ef0b6d69ed.lua', true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
